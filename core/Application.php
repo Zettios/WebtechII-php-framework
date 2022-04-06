32d4290438bd4d1703b027e5adbb2a61eed59c65
Zettios <77236807+Zettios@users.noreply.github.com>
2022-04-06 16:13:22 +0200

    feat(routing): Start with routing

---
+++ core/Application.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Webtek\Core;
+
+class Application {
+    public Router $router;
+    public Request $request;
+    public function __construct() {
+        $this->request = new Request();
+        $this->router = new Router($this->request);
+    }
+
+    public function run() {
+        $this->router->resolve();
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
 
+use Webtek\Core\Application;
+
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$app = new Application();
+
+$app->router->get('/', function(){
+    return 'Hello World';
+});
+
+$app->router->get('/contact', function(){
+    return 'Contact';
+});
+
+$app->run();
