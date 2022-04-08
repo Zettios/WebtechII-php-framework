2aa1d838d4fc47ef62255e814746e0f77a1fbb1a
Zettios <77236807+Zettios@users.noreply.github.com>
2022-04-08 15:49:25 +0200

    Nieuwe start

---
+++ core/Router.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Webtek\Core;
+
+class Router
+{
+
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
+use Webtek\Core\Request;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$request = (new Webtek\Core\Request)->getUri();
+
+var_dump($request['REQUEST_URI']);
